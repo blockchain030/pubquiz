74a652596a04fc892253e79bf5320c7221de71cc
Eric van Riet Paap <ericvrp@gmail.com>
2018-02-19 11:54:47 +0100

    cleanup

--- app/src/pages/tabs/tabs.ts
+++ app/src/pages/tabs/tabs.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
+import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
 
 @Component({
   templateUrl: 'tabs.html'
