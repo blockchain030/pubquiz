ac3742b013a2b7f27f7770b7f93dd0d8b4f4d307
Eric van Riet Paap <ericvrp@gmail.com>
2018-02-19 17:19:04 +0100

    Basic rate page

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { RatePage } from '../pages/rate/rate';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -19,6 +20,7 @@ import { Data } from '../providers/data';
     AboutPage,
     ContactPage,
     HomePage,
+    RatePage,
     TabsPage
   ],
   imports: [
@@ -31,6 +33,7 @@ import { Data } from '../providers/data';
     AboutPage,
     ContactPage,
     HomePage,
+    RatePage,
     TabsPage
   ],
   providers: [
--- app/src/pages/rate/rate.ts
+++ app/src/pages/rate/rate.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Data } from '../../providers/data';
-import { RatePage } from '../rate/rate';
-
-// let gPubquiz2 = require('../../assets/datasets/20180219-questions.json')
-// console.log(gPubquiz2)
 
 let gPubquiz = {
   "rounds": [
@@ -13,12 +9,14 @@ let gPubquiz = {
       {
         "number": "1",
         "question": "vraag 1",
-        "answer": "antwoord 1"
+        "answer": "antwoord 1",
+        "answers": [{userId: 100, approve: true, text: "Antwoord player 1"}, {userId: 101, approve: true, text: "Antwoord player 2"}, {userId: 102, approve: true, text: "Antwoord player3"}, {userId: 103, approve: true, text: "Antwoord player 4"}],
       },
       {
         "number": "2",
         "question": "vraag 2",
-        "answer": "antwoord 2"
+        "answer": "antwoord 2",
+        "answers": [{userId: 100, approve: true, text: "Antwoord player 1"}, {userId: 101, approve: true, text: "Antwoord player 2"}, {userId: 102, approve: true, text: "Antwoord player3"}, {userId: 103, approve: true, text: "Antwoord player 4"}],
       }
     ]},
     { "title": "ronde 2 - ethereum",
@@ -26,12 +24,14 @@ let gPubquiz = {
       {
         "number": "1",
         "question": "vraag 1",
-        "answer": "antwoord 1"
+        "answer": "antwoord 1",
+        "answers": [{userId: 100, approve: true, text: "Antwoord player 1"}, {userId: 101, approve: true, text: "Antwoord player 2"}, {userId: 102, approve: true, text: "Antwoord player3"}, {userId: 103, approve: true, text: "Antwoord player 4"}],
       },
       {
         "number": "2",
         "question": "vraag 2",
-        "answer": "antwoord 2"
+        "answer": "antwoord 2",
+        "answers": [{userId: 100, approve: true, text: "Antwoord player 1"}, {userId: 101, approve: true, text: "Antwoord player 2"}, {userId: 102, approve: true, text: "Antwoord player3"}, {userId: 103, approve: true, text: "Antwoord player 4"}],
       }
     ]},
   ]
@@ -41,17 +41,17 @@ let gPubquiz = {
 // import Web3 from 'web3';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-rate',
+  templateUrl: 'rate.html'
 })
-export class HomePage {
+export class RatePage {
 
   // web3: any;
   
   pubquiz: any;
   round: any;     // index
   question: any;  // index
-  answer: any[];
+  approval: any[];
 
   constructor(public navCtrl: NavController) {
     this.pubquiz = gPubquiz
@@ -81,6 +81,26 @@ export class HomePage {
       // });
   }
 
+  disapprove(userId) {
+    console.log('disapprove', userId)
+    for (let answer of this.pubquiz.rounds[this.round].questions[this.question].answers) {
+      if (answer.userId === userId) {
+        answer.approve = false;
+      }
+      console.log(answer)
+    }
+  }
+  
+  approve(userId) {
+    console.log('approve', userId)
+    for (let answer of this.pubquiz.rounds[this.round].questions[this.question].answers) {
+      if (answer.userId === userId) {
+        answer.approve = true;
+      }
+      console.log(answer)
+    }
+  }
+  
   previousQuestion() {
     this.question = Math.max(0, this.question-1) 
     // console.log('previousQuestion', this.question)
