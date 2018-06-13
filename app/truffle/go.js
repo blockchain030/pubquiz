ad4589bb17801cd672ed3e1f04c165e0aec5f67d
mosbuma <mosbuma@bumos.nl>
2018-06-13 10:24:21 +0200

    added error handling

--- app/truffle/go.js
+++ app/truffle/go.js
@@ -3,26 +3,26 @@
 
 module.exports = function(callback) {
 
-  // perform actions
-  var Pubquiz = artifacts.require("./contracts/Pubquiz.sol");
-  var instance;
+  try {
 
-  Pubquiz.deployed().then(_instance => {
-    return instance = _instance
-  }).then(()=>{
-    return instance.setTeam(web3.eth.accounts[0], 'Team Marc');
-  }).then(()=>{
-    return instance.setTeam(web3.eth.accounts[1], 'Team Susanne');
-  }).then(()=>{
-    return instance.setTeam(web3.eth.accounts[2], 'Team Eric');
-  }).then(res=>{
-    return instance.getTeamAccts();
-  }).then((res)=>{
-    console.log(res);
-  });
+    // perform actions
+    var Pubquiz = artifacts.require("./contracts/Pubquiz.sol");
+    var instance;
 
-  
-
-  // console.log(instance)
-
+    Pubquiz.deployed().then(_instance => {
+      return instance = _instance
+    }).then(()=>{
+      return instance.setTeam(web3.eth.accounts[0], 'Team Marc');
+    }).then(()=>{
+      return instance.setTeam(web3.eth.accounts[1], 'Team Susanne');
+    }).then(()=>{
+      return instance.setTeam(web3.eth.accounts[2], 'Team Eric');
+    }).then(res=>{
+      return instance.getTeamAccts();
+    }).then((res)=>{
+      console.log(res);
+    });
+  } catch(ex) {
+    console.log(ex);
+  }
 }
