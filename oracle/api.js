6d18c861ab02ec74a9a3a45a8b7d641b2f19e34d
mosbuma <mosbuma@bumos.nl>
2018-06-14 12:37:07 +0200

    work in progress on api

--- oracle/api.js
+++ oracle/api.js
@@ -4,32 +4,44 @@ const port = 3001; // process.env.PORT || 3030;
 
 const bodyParser = require('body-parser');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+try {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
-var pubquizOracle = require('./pubquizOracle');
+  var pubquizOracle = require('./pubquizOracle');
 
-// create a new quiz, store data on ipfs, setup smart contract, return smart contract address
-app.route('/quiz/create')
-  .get(pubquizOracle.createQuiz)
+  // create a new quiz, store data on ipfs, setup smart contract, return smart contract address
+  app.route('/quiz/create')
+    .get(pubquizOracle.createQuiz)
 
-// return hash of the current quiz smart contract address
-app.route('/quiz/getaddress')
-  .get(pubquizOracle.getAddress)
+  // return hash of the current quiz smart contract address
+  app.route('/quiz/getaddress')
+    .get(pubquizOracle.getAddress)
 
-// close answer round, close check round etc.
-app.route('/quiz/nextstep')
-  .get(pubquizOracle.nextStep)
+  // close answer round, close check round etc.
+  app.route('/quiz/nextstep')
+    .get(pubquizOracle.nextStep)
 
-  // app.route('/ipfs/getstatus')
-  //   .get(pubquizOracle.ipfsstatus)
+    // app.route('/ipfs/getstatus')
+    //   .get(pubquizOracle.ipfsstatus)
 
-app.listen(port);
+  const ts = new Date();
+  console.log(ts.toString("yyyy/MM/dd hh:mm:ss") + ' pubquiz oracle API server started on: ' + port);
 
-console.log('pubquiz oracle API server started on: ' + port);
+  app.listen(port);
+
+  const os = require('os');
+  console.log("Platform: " + os.platform());
+  console.log("Architecture: " + os.arch());
+  console.log("hostname: " + os.hostname());
+  console.log("userinfo: " + JSON.stringify(os.userInfo()));
+
+} catch(ex) {
+  console.log("api.js - fatal error" + ex.message);
+}
